2643822d087097f16b69df3c796faab93321d2fb
Robert Pedersen <rob@worldcrimeleague.org>
2019-07-30 13:17:17 -0400

    polls controller update

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,14 +1,19 @@
 class PollsController < ApplicationController
 
   def index
-    
+
     poll = Poll.all
-    
+
     render json: poll.to_json(
       :except => [:id, :created_at, :updated_at ],
       :include => {
         :poll_data_favorabilities => {
-          :except => [:id, :created_at, :updated_at]
+          :except => [:id, :poll_id, :candidate_id, :created_at, :updated_at],
+          :include => {
+            :candidate => {
+              :except => [:id, :created_at, :updated_at]
+            }
+          }
         }
       }
     )
@@ -23,7 +28,12 @@ class PollsController < ApplicationController
       :except => [:id, :created_at, :updated_at ],
       :include => {
         :poll_data_favorabilities => {
-          :except => [:id, :created_at, :updated_at]
+          :except => [:id,  :poll_id, :candidate_id, :created_at, :updated_at],
+          :include => {
+            :candidate => {
+              :except => [:id, :created_at, :updated_at]
+            }
+          }
         }
       }
     )
